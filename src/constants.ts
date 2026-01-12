37295301a8bdbf310fa7fb1ed3bbd09534983c40
Andrew Rowley <andrew@internetdrew.com>
2026-01-12 14:53:16 -0500

    add constants for better management

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const headline =
+  "I plan the party, so you get to enjoy it, not manage it.";
+export const subhead =
+  "For birthdays, dinners, intimate celebrations, and small company events that don't feel corporate.";
